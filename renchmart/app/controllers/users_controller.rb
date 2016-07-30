284bf19e978363bc24585d3a4df15d4826f081a2
Rencheng Chua <rench00@gmail.com>
2016-07-30 17:02:18 +0800

    finish sign-up and basic show user

--- renchmart/app/controllers/users_controller.rb
+++ renchmart/app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      # log_in @user
       flash[:success] = "Welcome In!!"
       redirect_to @user
     else
--- renchmart/config/routes.rb
+++ renchmart/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  # dynamic routes
+  get '/users', to: 'users#index'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+
   #RESTful routes
   resources :users
 
