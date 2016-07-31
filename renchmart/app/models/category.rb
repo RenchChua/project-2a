fc70b3fa3747b2fffdac65b8b81095c552ad1fe3
Rencheng Chua <rench00@gmail.com>
2016-07-31 17:18:31 +0800

    finish categories

---
+++ renchmart/app/models/category.rb
@@ -0,0 +1,14 @@
+class Category < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  mount_uploader :picture, PictureUploader
+  validate  :picture_size
+  default_scope -> { order(created_at: :desc) }
+
+  # Validates the size of an uploaded picture.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
+
+end
--- renchmart/config/routes.rb
+++ renchmart/config/routes.rb
@@ -10,12 +10,18 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get '/users/:id/edit', to: 'users#edit'
   patch '/users/:id/edit', to: 'users#update'
+  get '/categories', to: 'categories#index'
+  get '/categories/new', to: 'categories#new'
+  post '/categories/new', to: 'categories#create'
+  get '/categories/:id', to: 'categories#show'
+  get '/categories/:id/edit', to: 'categories#edit'
+  patch '/categories/:id/edit', to: 'categories#update'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
   #RESTful routes
   resources :users
-
+  resources :categories
 
 end
